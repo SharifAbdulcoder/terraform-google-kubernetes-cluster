fdae329f22676e98f5fe4f34292a3cd885826299
Tsadok Levi Firseck <tsadoklf@gmail.com>
2018-10-31 18:35:06 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,34 @@
+
+variable "cluster_name" {
+    type    = "string"
+}
+variable "network_name" {
+    type    = "string"
+}
+variable "subnetwork_name" {
+    type    = "string"
+}
+variable "zone" {
+    type    = "string"
+}
+variable "additional_zones" {
+    type    = "list"
+    default = []
+}
+variable "username" {
+    type    = "string"
+    default = ""
+}
+variable "password" {
+    type    = "string"
+    default = ""
+}
+
+variable "initial_node_count" {
+    type    = "string"
+    default = "1"
+}
+variable "machine_type" {
+    type    = "string"
+    default = "n1-standard-1"
+}
